e2f23e6651d57a6ec50f2ba04f2f2a7be7d4a3f4
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2019-01-29 03:51:24 -0300

    timido comienzo de uso de red Bayesiana

---
+++ bayesian.R
@@ -0,0 +1,15 @@
+
+
+library(readr)
+dataset <- read_csv("resultados_muestras.csv")
+dataset$Origen <- as.factor(dataset$Origen)
+
+library(bnlearn)
+res = hc(dataset)
+# plot the network structure.
+plot(res)
+
+
+res2 = iamb(dataset)
+# plot the new network structure.
+plot(res2)
