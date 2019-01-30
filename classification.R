713da18e7041d76bd31bbfa5385071816d0536db
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2019-01-30 14:04:18 -0300

    nnet + RF

--- classification.R
+++ classification.R
@@ -46,3 +46,46 @@ p <- predict(model.nnet$finalModel,x,type="class")
 print(table(p,y))
 
 plot(model.nnet)
+
+#' ## KNN o K nearest neighboors
+
+set.seed(SEED)
+mySeeds <- sapply(simplify = FALSE, 1:11, function(u) sample(10^4, 12))
+train_control <- trainControl(method="cv", number=10,seeds = mySeeds)
+
+model.knn <- train(as.factor(Origen)~., data=data, 
+                  trControl=train_control, method="knn",metric=METRIC,
+                  preProcess = c("center","scale"), tuneLength = 12) # laplace correction
+
+
+print(table(predict(model.knn$finalModel,x,type="class"),y))
+
+plot(model.knn)
+
+#' ## Random Forest
+#' 
+set.seed(SEED)
+model.rf <- train(as.factor(Origen)~., data=data, 
+                  trControl=train_control, method="rf",metric=METRIC, importance=T)
+
+#'  ### Results of random forest model
+print(model.rf)
+plot(model.rf)
+print(model.rf$finalModel)
+#' ### Variable importance
+varImp(model.rf)
+plot(varImp(model.rf))
+#' Predicción en conjunto de testeo test-set
+pred <- predict(model.rf,data)
+c <- confusionMatrix(as.factor(pred), as.factor(data$Origen),mode = "prec_recall")
+print(c)
+
+
+results <- resamples(list(RF=model.rf,NBayes=model.nb,knn=model.knn,nnet=model.nnet))
+# summarize the distributions
+summary(results)
+# boxplots of results
+bwplot(results)
+# dot plots of results
+dotplot(results)
+
