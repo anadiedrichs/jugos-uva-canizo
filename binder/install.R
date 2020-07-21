080ac51eff0103fe59d184f3705daee55ac0e43f
Ana Diedrichs (yoga) <ana.diedrichs@frm.utn.edu.ar>
2020-07-20 21:31:56 -0300

    related #1 - agrego soporte para Rstudio

---
+++ binder/install.R
@@ -0,0 +1,15 @@
+
+install.packages("tidyverse") 
+install.packages("DataExplorer")
+install.packages("GGally")
+install.packages("ggfortify")
+install.packages("factoextra")
+
+install.packages("cluster")
+
+install.packages("fpc")
+
+install.packages("DMwR")
+
+install.packages("caret")
+
