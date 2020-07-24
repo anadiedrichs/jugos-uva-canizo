b74ba9f3b2a234a1257382e255869953d2d9fa78
Ana Diedrichs (yoga) <ana.diedrichs@frm.utn.edu.ar>
2020-07-24 03:14:51 -0300

    related #2

---
+++ loadData.R
@@ -0,0 +1,25 @@
+library(stringr)
+library(readr)
+
+# normalizamos los datos entre 0 y 1
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+
+load_dataset <- function()
+{
+  dataset <- read_excel("../data/Dataset para enfoque 2 y 3.xlsx")
+  dataset <- dataset %>% filter(Origen != "CH")
+  
+  # quito el ppb al final del nombre de la columna
+  
+  colnames(dataset) <- str_extract(colnames(dataset), "[A-Z][a-z]*")
+  
+  dataset <- data.frame(dataset[,1], apply(dataset[-1],2,range01))
+  
+  return(dataset)
+}
+
+load_raw_data <- function()
+{
+  read_excel("../data/Dataset para enfoque 2 y 3.xlsx")
+}
